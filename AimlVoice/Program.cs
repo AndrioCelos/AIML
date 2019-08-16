f37a30df2a66e839c904c048b768c8511d619cbd
Andrio Celos <6897624+AndrioCelos@users.noreply.github.com>
2019-08-17 02:42:00 +1000

    AimlVoice: add --no-sr option and custom OOB handlers.

--- AimlVoice/Program.cs
+++ AimlVoice/Program.cs
@@ -14,7 +14,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AimlVoice {
-	class Program {
+	public class Program {
 		internal static SpeechRecognitionEngine recognizer;
 		internal static Bot bot;
 		internal static User user;
@@ -24,30 +24,66 @@ namespace AimlVoice {
 		internal static List<string> enabledGrammarPaths = new List<string>();
 		internal static bool partialInput;
 		private static Stopwatch partialInputTimeout = Stopwatch.StartNew();
+		public static Dictionary<string, Action<string>> OobHandlers { get; } = new Dictionary<string, Action<string>>(StringComparer.CurrentCultureIgnoreCase) {
+			{ "SetGrammar", oobSetGrammar },
+			{ "SetPartialInput", oobPartialInput }
+		};
 		
 		static int Main(string[] args) {
 			bool switches = true; string? botPath = null; var defaultGrammarPath = new List<string>();
 			string? voice = null; var sraixServicePaths = new List<string>();
+			int rate = 0, volume = 100;
+			bool sr = true;
 
 			for (int i = 0; i < args.Length; ++i) {
 				var s = args[i];
 				if (switches && s.StartsWith("-")) {
-					if (s == "--")
-						switches = false;
-					else if (s == "-h" || s == "--help" || s == "/?") {
-						Console.WriteLine("Usage: AimlVoice [switches] <bot path>");
-						Console.WriteLine("Available switches:");
-						Console.WriteLine("  -g [name], --grammar [name]: Enable the specified grammar upon startup. Specify a file name in the `grammars` directory without the `.xml` extension.");
-						Console.WriteLine("  -s [path], --services [path]: Load AIML services from the specified library.");
-						Console.WriteLine("  -v [name], --voice [name]: Use the specified voice. If invalid, a list of available voices will be shown.");
-						Console.WriteLine("  --: Stop processing switches.");
-						return 0;
-					} else if (s == "-g" || s == "--grammar")
-						defaultGrammarPath.Add(args[++i]);
-					else if (s == "-s" || s == "--services")
-						sraixServicePaths.Add(args[++i]);
-					else if (s == "-v" || s == "--voice")
-						voice = args[++i];
+					switch (s) {
+						case "--":
+							switches = false;
+							break;
+						case "-h":
+						case "--help":
+						case "/?":
+							Console.WriteLine("Usage: AimlVoice [switches] <bot path>");
+							Console.WriteLine("Available switches:");
+							Console.WriteLine("  -g [name], --grammar [name]: Enable the specified grammar upon startup. Specify a file name in the `grammars` directory without the `.xml` extension.");
+							Console.WriteLine("  -S [path], --services [path]: Load AIML services from the specified library.");
+							Console.WriteLine("  -V [name], --voice [name]: Use the specified voice. If invalid, a list of available voices will be shown.");
+							Console.WriteLine("  -r [number], --rate [number]: Modify the speech rate. -10 ~ +10; default is 0.");
+							Console.WriteLine("  -v [number], --volume [number]: Modify the speech volume. -10 ~ +10; default is 0.");
+							Console.WriteLine("  -n, --no-sr: Do not load the speech recogniser. Input will by typing only.");
+							Console.WriteLine("  --: Stop processing switches.");
+							return 0;
+						case "-g":
+						case "--grammar":
+							defaultGrammarPath.Add(args[++i]);
+							break;
+						case "-S":
+						case "--services":
+							sraixServicePaths.Add(args[++i]);
+							break;
+						case "-V":
+						case "--voice":
+							voice = args[++i];
+							break;
+						case "-v":
+						case "--volume":
+							volume = int.Parse(args[++i]);
+							break;
+						case "-r":
+						case "--rate":
+							rate = int.Parse(args[++i]);
+							break;
+						case "-n":
+						case "--no-sr":
+							sr = false;
+							break;
+						default:
+							Console.Error.WriteLine("Unknown switch " + s);
+							Console.Error.WriteLine("Use `AimlVoice --help` for more information.");
+							return 1;
+					}
 				} else {
 					switches = false;
 					botPath = s;
@@ -85,7 +121,7 @@ namespace AimlVoice {
 
 			bot.LoadConfig();
 			bot.LoadAIML();
-			bot.Config.LogLevel = LogLevel.Info;
+			//bot.Config.LogLevel = LogLevel.Info;
 
 			user = new User("User", bot);
 			synthesizer = new SpeechSynthesizer();
@@ -100,45 +136,56 @@ namespace AimlVoice {
 					return 1;
 				}
 			}
-			synthesizer.Rate = 1;
 
-			using (recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en")) {
-				BabbleTimeout = TimeSpan.FromSeconds(1),
-				EndSilenceTimeoutAmbiguous = TimeSpan.FromSeconds(0.75)
-			}) {
+			synthesizer.Rate = rate;
+			synthesizer.Volume = volume;
 
-				foreach (var entry in grammars) {
-					Console.WriteLine($"Loading grammar '{entry.Key}'...");
-					entry.Value.Enabled = false;
-					recognizer.LoadGrammar(entry.Value);
-				}
-
-				if (defaultGrammarPath.Count == 0) {
-					Console.WriteLine($"Loading dictation grammar...");
-					enabledGrammarPaths.Add("");
-					grammars[""] = new DictationGrammar();
-					recognizer.LoadGrammar(grammars[""]);
-				} else {
-					foreach (var name in defaultGrammarPath) {
-						enabledGrammarPaths.Add(name);
-						grammars[name].Enabled = true;
-					}
-				}
-
-				recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-				recognizer.SpeechRecognitionRejected += Recognizer_SpeechRecognitionRejected;
-				recognizer.SpeechHypothesized += Recognizer_SpeechHypothesized;
-				recognizer.RecognizerUpdateReached += Recognizer_RecognizerUpdateReached;
-
-				recognizer.SetInputToDefaultAudioDevice();
-
-				recognizer.RecognizeAsync(RecognizeMode.Multiple);
-
+			if (!sr) {
 				Console.Write("> ");
 				while (true) {
 					var message = Console.ReadLine();
 					sendInput(message);
 					Console.Write("> ");
+				}
+			} else {
+				using (recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en")) {
+					BabbleTimeout = TimeSpan.FromSeconds(1),
+					EndSilenceTimeoutAmbiguous = TimeSpan.FromSeconds(0.75)
+				}) {
+
+					foreach (var entry in grammars) {
+						Console.WriteLine($"Loading grammar '{entry.Key}'...");
+						entry.Value.Enabled = false;
+						recognizer.LoadGrammar(entry.Value);
+					}
+
+					if (defaultGrammarPath.Count == 0) {
+						Console.WriteLine($"Loading dictation grammar...");
+						enabledGrammarPaths.Add("");
+						grammars[""] = new DictationGrammar();
+						recognizer.LoadGrammar(grammars[""]);
+					} else {
+						foreach (var name in defaultGrammarPath) {
+							enabledGrammarPaths.Add(name);
+							grammars[name].Enabled = true;
+						}
+					}
+
+					recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+					recognizer.SpeechRecognitionRejected += Recognizer_SpeechRecognitionRejected;
+					recognizer.SpeechHypothesized += Recognizer_SpeechHypothesized;
+					recognizer.RecognizerUpdateReached += Recognizer_RecognizerUpdateReached;
+
+					recognizer.SetInputToDefaultAudioDevice();
+
+					recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+					Console.Write("> ");
+					while (true) {
+						var message = Console.ReadLine();
+						sendInput(message);
+						Console.Write("> ");
+					}
 				}
 			}
 		}
@@ -165,20 +212,19 @@ namespace AimlVoice {
 			writeMessage($"({e.Result.Text} ... {e.Result.Confidence})");
 			Console.ResetColor();
 
-			if (partialInput && partialInputTimeout.Elapsed >= TimeSpan.FromSeconds(3) && e.Result.Confidence
->= 0.25) {
-					var response = bot.Chat(new Request("PartialInput " + e.Result.Text, user, bot), false);
-					var text = Regex.Replace(response.ToString(), "<oob>([^<]*)</oob>", "");
-					if (!string.IsNullOrWhiteSpace(text)) {
-							partialInputTimeout.Restart();
-							Console.ForegroundColor = ConsoleColor.Blue;
-							Console.WriteLine();
-							Console.WriteLine(text);
-							Console.ResetColor();
-							Console.Write("> ");
-							synthesizer.SpeakAsyncCancelAll();
-							synthesizer.SpeakAsync(text);
-					}
+			if (partialInput && partialInputTimeout.Elapsed >= TimeSpan.FromSeconds(3) && e.Result.Confidence >= 0.25) {
+				var response = bot.Chat(new Request("PartialInput " + e.Result.Text, user, bot), false);
+				var text = Regex.Replace(response.ToString(), "<oob>([^<]*)</oob>", "");
+				if (!string.IsNullOrWhiteSpace(text)) {
+					partialInputTimeout.Restart();
+					Console.ForegroundColor = ConsoleColor.Blue;
+					Console.WriteLine();
+					Console.WriteLine(text);
+					Console.ResetColor();
+					Console.Write("> ");
+					synthesizer.SpeakAsyncCancelAll();
+					synthesizer.SpeakAsync(text);
+				}
 			}
 		}
 
@@ -197,42 +243,49 @@ namespace AimlVoice {
 			}
 		}
 
-		private static void sendInput(string input) {
+		public static void sendInput(string input) {
 			var trace = false;
 			if (input.StartsWith(".trace ")) {
 				trace = true;
 				input = input.Substring(7);
 			}
 			var response = bot.Chat(new Request(input, user, bot), trace);
-			var text = Regex.Replace(response.ToString(), "<oob>([^<]*)</oob>", m => {
-				var fields = m.Groups[1].Value.Split((char[]?) null, StringSplitOptions.RemoveEmptyEntries);
-				switch (fields[0]) {
-					case "SetGrammar":
-						var name = fields.Length > 1 ? fields[1] : "";
-						if (!enabledGrammarPaths.Contains(name)) {
-							if (grammars.TryGetValue(name, out var grammar)) {
-								Console.WriteLine($"Switching to grammar '{name}'");
-								foreach (var path in enabledGrammarPaths)
-									grammars[path].Enabled = false;
-								enabledGrammarPaths.Clear();
-								grammar.Enabled = true;
-								enabledGrammarPaths.Add(name);
-							} else
-								Console.WriteLine($"Could not find requested grammar '{name}'.");
-						}
-						break;
-					case "SetPartialInput":
-						partialInput = bool.Parse(fields[1]);
-						Console.WriteLine($"Partial input is {(partialInput ? "enabled" : "disabled")}.");
-						break;
-				}
+			var queue = false;
+			var text = Regex.Replace(response.ToString(), "<oob>(.*?)</oob>", m => {
+				var fields = m.Groups[1].Value.Trim().Split((char[]?) null, 2, StringSplitOptions.RemoveEmptyEntries);
+				if (OobHandlers.TryGetValue(fields[0], out var action))
+					action.Invoke(fields.Length == 1 ? "" : fields[1].TrimEnd());
+				else if (fields[0].Equals("queue", StringComparison.CurrentCultureIgnoreCase))
+					queue = true;
 				return "";
 			});
 			Console.ForegroundColor = ConsoleColor.Blue;
 			Console.WriteLine(text);
 			Console.ResetColor();
-			synthesizer.SpeakAsyncCancelAll();
-			synthesizer.SpeakAsync(text);
+			if (!string.IsNullOrWhiteSpace(text)) {
+				if (!queue)
+					synthesizer.SpeakAsyncCancelAll();
+				synthesizer.SpeakAsync(text);
+			}
+		}
+
+		private static void oobPartialInput(string text) {
+			partialInput = bool.Parse(text);
+			Console.WriteLine($"Partial input is {(partialInput ? "enabled" : "disabled")}.");
+		}
+
+		private static void oobSetGrammar(string text) {
+			if (!enabledGrammarPaths.Contains(text)) {
+				if (grammars.TryGetValue(text, out var grammar)) {
+					Console.WriteLine($"Switching to grammar '{text}'");
+					foreach (var path in enabledGrammarPaths)
+						grammars[path].Enabled = false;
+					enabledGrammarPaths.Clear();
+					grammar.Enabled = true;
+					enabledGrammarPaths.Add(text);
+				} else
+					Console.WriteLine($"Could not find requested grammar '{text}'.");
+			}
 		}
 
 		private static void Bot_LogMessage(object sender, LogMessageEventArgs e) {
