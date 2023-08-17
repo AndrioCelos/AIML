bd9f4ddbd56a5133b110a889041df210bd94fb13
Andrio Celos <AndrioCelos@users.noreply.github.com>
2023-08-17 12:57:47 +1000

    Fix ISraixService documentation

--- Aiml/ISraixService.cs
+++ Aiml/ISraixService.cs
@@ -1,11 +1,12 @@
 ﻿using System.Xml;
 
 namespace Aiml; 
-/// <summary>Implements a handler for the AIML <code>sraix</code> template element.</summary>
+/// <summary>Implements a handler for the AIML <c>sraix</c> template element.</summary>
 public interface ISraixService {
 	/// <summary>Processes a request and returns the result.</summary>
-	/// <param name="text">The evaluated contents of the <code>sraix</code> element.</param>
-	/// <param name="attributes">The attributes of the <code>sraix</code> element.</param>
-	/// <param name="subRequest">The sub-request currently being processed.</param>
+	/// <param name="text">The evaluated contents of the <c>sraix</c> element.</param>
+	/// <param name="attributes">The attributes of the <c>sraix</c> element.</param>
+	/// <param name="process">A <see cref="RequestProcess"/> instance providing information about the request currently being processed.</param>
+	/// <returns>The text that should replace the <c>sraix</c> element.</returns>
 	string Process(string text, XmlAttributeCollection attributes, RequestProcess process);
 }
